07cfda072645ba51ecdfe4e724551042204c2991
dumasj2atwit <dumasj2@wit.edu>
2026-06-01 22:21:34 -0400

    added database connectivity

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,39 @@
 from fastapi import FastAPI
+from database import connection
+from sqlalchemy import text
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
-    return {"message": "the GRIPS backend is running"}
+    return {"message": "GRIPS backend running"}
+
 
 @app.get("/health")
-def health_check():
-    return {"status": "ok"}
+def health():
+
+    return {
+        "status": "ok"
+    }
+
+
+@app.get("/ping")
+def ping():
+
+    return {
+        "message": "pong"
+    }
+
+
+@app.get("/routes")
+def get_routes():
+
+    result = connection.execute(text("SELECT * FROM routes;"))
+
+    routes = []
+
+    for row in result:
+        routes.append(dict(row._mapping))
+
+    return routes
