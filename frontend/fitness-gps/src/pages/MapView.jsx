0ed7767e12f033598e07a0a75cff0e91b61fe35e
tollisa <austinjtollis@yahoo.com>
2026-06-22 15:32:21 -0400

    Whitescreen fix
    Fixed a whitescreen error

--- frontend/fitness-gps/src/pages/MapView.jsx
+++ frontend/fitness-gps/src/pages/MapView.jsx
@@ -104,75 +104,76 @@ function MapView() {
     } finally {
       setLoading(false);
     }
-    const updateRouteProgress = () => {
+  };
 
-      const coordinates =
-        route.features[0].geometry.coordinates;
+  const updateRouteProgress = () => {
 
-      // Find the route point closest to the user
-      let closestIndex = 0;
-      let shortestDistance = Infinity;
+    const coordinates =
+      route.features[0].geometry.coordinates;
 
-      coordinates.forEach((point, index) => {
+    // Find the route point closest to the user
+    let closestIndex = 0;
+    let shortestDistance = Infinity;
 
-        const lng = point[0];
-        const lat = point[1];
+    coordinates.forEach((point, index) => {
 
-        const distance =
-          Math.sqrt(
-            (lat - coords.latitude) ** 2 +
-            (lng - coords.longitude) ** 2
-          );
+      const lng = point[0];
+      const lat = point[1];
 
-        if (distance < shortestDistance) {
+      const distance =
+        Math.sqrt(
+          (lat - coords.latitude) ** 2 +
+          (lng - coords.longitude) ** 2
+        );
 
-          shortestDistance = distance;
+      if (distance < shortestDistance) {
 
-          closestIndex = index;
-        }
-      });
+        shortestDistance = distance;
 
-      const completed = coordinates.slice(
-        0,
-        closestIndex + 1
-      );
+        closestIndex = index;
+      }
+    });
 
-      const upcoming = coordinates.slice(
-        closestIndex
-      );
+    const completed = coordinates.slice(
+      0,
+      closestIndex + 1
+    );
 
-      setCompletedRoute({
-        type: "FeatureCollection",
+    const upcoming = coordinates.slice(
+      closestIndex
+    );
 
-        features: [
-          {
-            type: "Feature",
+    setCompletedRoute({
+      type: "FeatureCollection",
 
-            geometry: {
-              type: "LineString",
+      features: [
+        {
+          type: "Feature",
 
-              coordinates: completed,
-            },
+          geometry: {
+            type: "LineString",
+
+            coordinates: completed,
           },
-        ],
-      });
+        },
+      ],
+    });
 
-      setUpcomingRoute({
-        type: "FeatureCollection",
+    setUpcomingRoute({
+      type: "FeatureCollection",
 
-        features: [
-          {
-            type: "Feature",
+      features: [
+        {
+          type: "Feature",
 
-            geometry: {
-              type: "LineString",
+          geometry: {
+            type: "LineString",
 
-              coordinates: upcoming,
-            },
+            coordinates: upcoming,
           },
-        ],
-      });
-    };
+        },
+      ],
+    });
   };
 
   return (//This down makes all the UI visuals for the map view page
